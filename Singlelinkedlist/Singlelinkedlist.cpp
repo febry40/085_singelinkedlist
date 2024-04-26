fe8c98bf78c4ade669141d699326ad6ecb7094ab
febry40 <febryevayonisa@gmail.com>
2024-04-26 15:42:50 +0700

    pengelolaan data dalam suatu struktur data

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -120,8 +120,41 @@ void searchData() {
 		Node* currentNode = START;
 		while (currentNode != NULL) {
 			if (currentNode->noMhs == nim) {
-				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name <<
-endl;
+				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "data tidak ditemukan" << endl;
+	}
+}
+
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			}
+		}
+	}
+}
 
 
 
