8f807a4166d3d50900592a6f4c86afef1a17d5fd
febry40 <febryevayonisa@gmail.com>
2024-04-26 14:58:49 +0700

    sebuah fungsi untuk menghapus sebuah node dalam linked list

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -57,9 +57,29 @@ bool serachNode(int nim, Node* current, Node* previous) {
 	{
 		previous = current;
 		current = current->next;
-    }
+	}
 
 	if (current == NULL)
 	{
 		return false;
 	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+} 
+bool deleteNode(int nim) {
+	Node* current = START;
+	Node* previous = START;
+	if (serachNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
+}
+
